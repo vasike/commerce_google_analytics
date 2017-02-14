34fd232567b1a4ffa9288ef077a2ee2493a83dbd
tavi toporjinschi <vasike@gmail.com>
2017-02-14 13:17:20 +0000

    First implementation for sending Ecommerce tracking data to google analytics when an order is placed - using an EventSubscriber.

---
+++ src/EventSubscriber/SendOrderAnalyticsSubscriber.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Drupal\commerce_google_analytics\EventSubscriber;
+
+use Drupal\commerce_order\Entity\OrderInterface;
+use Drupal\commerce_product\Entity\ProductVariationInterface;
+use Drupal\state_machine\Event\WorkflowTransitionEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Sends Ecommerce tracking data to google analytics when an order is placed.
+ */
+class SendOrderAnalyticsSubscriber implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  static function getSubscribedEvents() {
+    $events['commerce_order.place.post_transition'] = ['sendOrderAnalytics', -100];
+
+    return $events;
+  }
+
+  /**
+   * Sends the Ecommerce tracking data using GA Push API.
+   *
+   * @param \Drupal\state_machine\Event\WorkflowTransitionEvent $event
+   *   The event we subscribed to.
+   */
+  public function sendOrderAnalytics(WorkflowTransitionEvent $event) {
+    /** @var \Drupal\commerce_order\Entity\OrderInterface $order */
+    $order = $event->getEntity();
+
+    // Build the GA Push array.
+    $ga_push_params = $this->buildGaPushParams($order);
+
+    // Push a GA ecommerce using GA Push module API.
+    // @see ga_push_add_ecommerce()
+    ga_push_add_ecommerce($ga_push_params);
+  }
+
+  /**
+   * Builds the Ecommerce Tracking data array needed by GA Push API for a
+   * given order.
+   *
+   * @param \Drupal\commerce_order\Entity\OrderInterface $order
+   *   The order.
+   */
+  public function buildGaPushParams(OrderInterface $order) {
+    $order_total = $order->getTotalPrice();
+    /** @var \Drupal\profile\Entity\ProfileInterface $billing_profile */
+    $billing_profile = $order->getBillingProfile();
+    /** @var \Drupal\address\AddressInterface $address */
+    $address = $billing_profile->get('address')->first();
+    // ToDo: tax and shipping totals.
+    $tax_total = 0;
+    $shipping_total = 0;
+
+    // Build the transaction array.
+    $transaction = [
+      'order_id' => $order->id(),
+      'affiliation' => $order->getStore()->label(),
+      'total' => $order_total->getNumber(),
+      'currency' => $order_total->getCurrencyCode(),
+      'total_tax' => $tax_total,
+      'total_shipping' => $shipping_total,
+      'city' => $address->getLocality(),
+      'region' => $address->getAdministrativeArea(),
+      'country' => $address->getCountryCode(),
+    ];
+
+    $items = [];
+    foreach ($order->getItems() as $order_item) {
+      $purchased_entity = $order_item->getPurchasedEntity();
+      // Build the item arguments.
+      $item = [
+        'order_id' => $order->id(),
+        'sku' => $order_item->id(),
+        'name' => $order_item->label(),
+        'category' => ucfirst($order_item->bundle()),
+        'price' => $order_item->getUnitPrice()->getNumber(),
+        'currency' => $order_item->getUnitPrice()->getCurrencyCode(),
+        'quantity' => $order_item->getQuantity(),
+      ];
+      // Check if purchased entity is a product variation and update item data.
+      if ($purchased_entity instanceof ProductVariationInterface) {
+        $item['sku'] = $purchased_entity->getSku();
+        $item['sku'] = 'Product: ' . $purchased_entity->bundle();
+      }
+
+      $items[] = $item;
+    }
+
+    $ga_push_params = [
+      'trans' => $transaction,
+      'items' => $items,
+    ];
+
+    return $ga_push_params;
+  }
+
+}
